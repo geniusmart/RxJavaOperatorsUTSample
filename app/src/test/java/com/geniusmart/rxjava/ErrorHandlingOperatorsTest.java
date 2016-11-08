a93f94ce6e99cd4589bbd6cd72254356f6c2d424
geniusmart <270391484@qq.com>
2016-11-08 20:20:54 +0800

    commit

--- app/src/test/java/com/geniusmart/rxjava/ErrorHandlingOperatorsTest.java
+++ app/src/test/java/com/geniusmart/rxjava/ErrorHandlingOperatorsTest.java
@@ -15,6 +15,7 @@ import rx.functions.Func2;
 import rx.schedulers.TestScheduler;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by geniusmart on 16/11/6.
@@ -54,35 +55,121 @@ public class ErrorHandlingOperatorsTest {
                     mList.add(integer);
                 }, throwable -> System.out.println(throwable.getMessage()));
 
-        assertEquals(mList, Arrays.asList(1,2,1,2,1,2));
+        assertEquals(mList, Arrays.asList(1, 2, 1, 2, 1, 2));
     }
 
+    /**
+     * 破坏数据流
+     * 有关repeatWhen的参考文章：http://www.jianshu.com/p/023a5f60e6d0
+     */
     @Test
-    public void retryWhen(){
-        Observable.create((Subscriber<? super String> s) -> {
+    public void retryWhen_break_sequence() {
 
-            int i = 0;
+        // 错误的做法：破坏数据流,打断链式结构
+        Observable.just(1, 2, 3)
+                .retryWhen(throwableObservable -> Observable.just(1, 1, 1))
+                .subscribe(mList::add);
+        //数据流被打断，订阅不到数据
+        assertTrue(mList.isEmpty());
+
+        // 正确的做法：至少将throwableObservable作为返回结果，此时的retryWhen()等价于retry()
+        Observable.just(1, 2, 3)
+                .retryWhen(throwableObservable -> throwableObservable).
+                subscribe(mList::add);
+        //此处的数据流不会触发error，因此正常输出1,2,3的数列
+        assertEquals(mList, Arrays.asList(1, 2, 3));
+    }
+
+    /**
+     * 使用retryWhen() + flatMap() + timer() 实现延迟重新订阅
+     */
+    @Test
+    public void retryWhen_flatMap_timer() {
+
+        Observable.create(subscriber -> {
             System.out.println("subscribing");
-            System.out.println(5/i++);
-            //s.onError(new RuntimeException("always fails"));
-        }).retryWhen(new Func1<Observable<? extends Throwable>, Observable<?>>() {
-            @Override
-            public Observable<?> call(Observable<? extends Throwable> observable) {
-                return observable.zipWith(Observable.range(1, 3), new Func2<Throwable, Integer, Integer>() {
-                    @Override
-                    public Integer call(Throwable throwable, Integer integer) {
-                        System.out.println(throwable.getMessage());
-                        return integer;
-                    }
-                }).flatMap(new Func1<Integer, Observable<?>>() {
-                    @Override
-                    public Observable<?> call(Integer o) {
-                        System.out.println("delay retry by " + o + " second(s)");
-                        return Observable.timer(o,TimeUnit.SECONDS);
-                    }
+            subscriber.onNext(1);
+            subscriber.onNext(2);
+            subscriber.onError(new RuntimeException("RuntimeException"));
+        })
+                .retryWhen(observable ->
+                        observable.flatMap(
+                                (Func1<Throwable, Observable<?>>) throwable ->
+                                        //延迟5s重新订阅
+                                        Observable.timer(5, TimeUnit.SECONDS, mTestScheduler)
+                        )
+                )
+                .subscribe(num -> {
+                    System.out.println(num);
+                    mList.add(num);
                 });
+
+        //时间提前10s，将发生1次订阅+2次重新订阅
+        mTestScheduler.advanceTimeBy(10, TimeUnit.SECONDS);
+
+        assertEquals(mList, Arrays.asList(1, 2, 1, 2, 1, 2));
+    }
+
+    /**
+     * TODO：使用zip() + range()实现有限次数的重订阅
+     */
+    @Test
+    public void retryWhen_zip_range() {
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                System.out.println("subscribing");
+                subscriber.onNext(1);
+                subscriber.onNext(2);
+                subscriber.onError(new RuntimeException("always fails"));
             }
-        }).toBlocking().forEach(System.out::println);
+        })
+                .retryWhen(new Func1<Observable<? extends Throwable>, Observable<?>>() {
+                    @Override
+                    public Observable<?> call(Observable<? extends Throwable> throwable) {
+                        return throwable.zipWith(Observable.range(1, 3), new Func2<Throwable, Integer, Object>() {
+                            @Override
+                            public Object call(Throwable throwable, Integer integer) {
+                                System.out.println("--->" + integer);
+                                return integer;
+                            }
+                        });
+                    }
+                })
+                .doOnNext(System.out::println)
+                .doOnCompleted(() -> System.out.println("completed"))
+                .subscribe(mList::add);
+
+        assertEquals(mList, Arrays.asList(1, 2, 1, 2, 1, 2, 1, 2));
+    }
+
+    /**
+     * 延迟策略与次数限制的重试机制结合起来
+     */
+    @Test
+    public void retryWhen_zip_range_timer() {
+
+        Observable.create((Subscriber<? super Integer> subscriber) -> {
+            System.out.println("subscribing");
+            subscriber.onNext(1);
+            subscriber.onNext(2);
+            subscriber.onError(new RuntimeException("always fails"));
+        })
+                .doOnNext(System.out::println)
+                .retryWhen(observable ->
+                        observable.zipWith(
+                                Observable.range(1, 3),
+                                (Func2<Throwable, Integer, Integer>) (throwable, integer) -> integer
+                        )
+                                .flatMap((Func1<Integer, Observable<?>>) num -> {
+                                    System.out.println("delay retry by " + num + " second(s)");
+                                    return Observable.timer(num, TimeUnit.SECONDS);
+                                }))
+                .toBlocking()
+                .forEach(mList::add);
+
+        //订阅一次，重新订阅3次
+        assertEquals(mList, Arrays.asList(1, 2, 1, 2, 1, 2, 1, 2));
     }
 
 }
